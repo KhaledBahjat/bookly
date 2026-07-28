9f529632e7eecfce516cf5b4b59242f04f6b604d
Khaled <khaledbahjat0@gmail.com>
2026-07-27 20:32:27 -0700

    update padding in CoustomAppBar for improved layout

--- lib/Features/home/presentation/widgets/coustom_app_bar.dart
+++ lib/Features/home/presentation/widgets/coustom_app_bar.dart
@@ -9,7 +9,7 @@ class CoustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 35, horizontal: 16),
+      padding: EdgeInsets.symmetric(vertical: 40, horizontal: 16),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
