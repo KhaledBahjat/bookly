17e66f703efcb1c8c7f699c94e1fa146dc0be64d
Khaled Bahjat <khaledbahjat0@gmail.com>
2026-08-11 07:11:01 +0300

    create search view

--- lib/core/routing/app_routs.dart
+++ lib/core/routing/app_routs.dart
@@ -2,4 +2,5 @@ class AppRouts {
   static const String homeView = '/homeView';
   static const String bookDetailsView = '/bookDetailsView';
   static const String splachView = '/splashView';
+  static const String searchView = '/searchView';
 }
--- lib/core/routing/routing_gen.dart
+++ lib/core/routing/routing_gen.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/Features/Splash/presentation/views/splash_view.dart';
 import 'package:bookly/Features/home/presentation/views/book_details_view.dart';
 import 'package:bookly/Features/home/presentation/views/home_view.dart';
+import 'package:bookly/Features/search/presentation/views/search_view.dart';
 import 'package:bookly/core/routing/app_routs.dart';
 import 'package:go_router/go_router.dart';
 
@@ -12,6 +13,11 @@ class RouterGenerator {
         path: AppRouts.splachView,
         builder: (context, state) => const SplashView(),
         name: AppRouts.splachView,
+      ),
+      GoRoute(
+        path: AppRouts.searchView,
+        builder: (context, state) => const SearchView(),
+        name: AppRouts.searchView,
       ),
       GoRoute(
         path: AppRouts.homeView,
